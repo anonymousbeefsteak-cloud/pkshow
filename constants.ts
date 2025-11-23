20933001de044644f0d5ce5797211c2e848c70dc
anonymousbeefsteak-cloud <anonymousbeefsteak@gmail.com>
2025-11-23 22:11:51 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -71,7 +71,7 @@ export const MENU_DATA: MenuCategory[] = [
           description: "附:①日湯②麵包③主餐④脆薯⑤飲料", 
           customizations: { doneness: true, sauceChoice: true, drinkChoice: true, notes: true, saucesPerItem: 2 }, 
           isAvailable: true,
-          image: 'https://anonymousbeefsteak-cloud.github.io/pkshow/b1.png'
+          image: 'https://anonymousbeefsteak-cloud.github.io/pkshow/b1.jpg'
       },
       { 
           id: 'set-7', 
@@ -83,7 +83,7 @@ export const MENU_DATA: MenuCategory[] = [
           description: "附:①日湯②麵包③主餐④脆薯⑤飲料", 
           customizations: { doneness: true, sauceChoice: true, drinkChoice: true, notes: true, saucesPerItem: 2 }, 
           isAvailable: true,
-          image: 'https://anonymousbeefsteak-cloud.github.io/pkshow/b1.jpg'
+          image: 'https://anonymousbeefsteak-cloud.github.io/pkshow/cap.png'
       },
       { 
           id: 'set-8', 
